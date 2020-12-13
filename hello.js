eeef118a697f64dbb82bd34f16401a1e2e9ed2bb
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-13 02:05:59 +0200

    Bug Fixes: multiple button clicks

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
 function Start(){
-    $("label").toggleClass("bg",1000);
+    //$("label").toggleClass("bg",1000);
+    $("label").addClass("bg",1000);
 }
 
 function Stop(){
-      $("label").toggleClass("bg",1000);
+    //$("label").toggleClass("bg",1000);
+    $("label").removeClass("bg",1000);
 }
