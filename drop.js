ff45ef8c3bb8b019ae0089194b27a910b96fe10e
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-13 05:02:32 +0200

    Added Bunny Effect

--- drop.js
+++ drop.js
@@ -1,8 +1,16 @@
 $("#drg").draggable();
 
-$( "#drp" ).droppable({
-  drop: function() {
-    //alert("hello");
-    $("#drg").fadeOut(1000);
-  }
+$("#drp").droppable({
+    drop: function () {
+        //alert("hello");
+        $("#drg").fadeOut(1000);
+    }
 });
+
+$("#drg").mouseenter(function () {
+    $("#dv").effect( "bounce", 400 );
+    $("#dv").effect( "shake", 400 );
+    $("#dv").effect( "bounce", 400 );
+});
+
+
